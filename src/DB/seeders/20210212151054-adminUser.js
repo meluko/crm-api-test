46958d2b575f9207a4521e74053cb67c17741525
Melvin Gutiérrez <melvin.gutierrez-rivero@gmail.com>
2021-02-15 18:00:51 +0000

    control duplicates in sequelize seeds

--- src/DB/seeders/20210212151054-adminUser.js
+++ src/DB/seeders/20210212151054-adminUser.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const axios = require('axios').default;
+const assert = require('assert');
+
 const GITHUB_USER = 'https://api.github.com/users';
 
 const getGithubUserData = function (githubUserName) {
@@ -9,14 +11,22 @@ const getGithubUserData = function (githubUserName) {
 
 module.exports = {
   up: async queryInterface => {
-    const {data: {id, login, name}} = await getGithubUserData(process.env.GITHUB_USER);
-    await queryInterface.bulkInsert('user', [{
+    const {GITHUB_USER} = process.env;
+    assert(GITHUB_USER, 'Env var GITHUB_USER is required');
+    const {data: {id, login, name}} = await getGithubUserData(GITHUB_USER);
+    const userData = {
       name: 'SuperAdmin',
       surname: name,
       githubLogin: login,
       githubId: id,
       isAdmin: true
-    }], {});
+    };
+    const user = await queryInterface.rawSelect('User', {where: userData,}, ['id']);
+    if (!user) {
+      await queryInterface.insert('user', userData, {});
+    } else {
+      console.info(`User ${GITHUB_USER} already exists`)
+    }
   },
 
   down: async queryInterface => {
